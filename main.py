f15e0fb368992e0b5c6faa911c8263369f5cd939
Joao Paulo Prata Costa <joaoppcosta@hotmail.com.br>
2022-01-26 17:59:28 -0300

    Arquivo usado para aprender biblioteca pynput

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from pynput.mouse import Button, Controller
+from fastapi import FastAPI
+import uvicorn
 
-print("Teste")
+app = FastAPI()
 
-mouse = Controller()
-mouse.position = (0, 0)
+@app.get('/')
+def raiz():
+    mouse = Controller()
+    mouse.move(10,0)
+
+#mouse.position = (0, 0)
+# mouse.click(Button.left, 2);
+# mouse.press(Button.left);
+# mouse.release(Button.left);
+# mouse.scroll(0, -100);
